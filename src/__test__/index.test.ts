1038ef835c7c6668ea065724f3321ff9808020c0
simon-tanna <gcab022109@coderacademy.edu.au>
2023-04-23 13:32:12 +1000

    refactor: implement interval and message count options. update tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -17,26 +17,68 @@ describe("getAllMessages", () => {
 });
 
 describe("getRandomMessage", () => {
-  it("returns a string", () => {
-    const message = getRandomMessage();
-    expect(typeof message === "string").toBe(true);
-  });
+  const originalSetInterval = global.setInterval;
+  const originalClearInterval = global.clearInterval;
 
-  it("returns a message that is included in the array", () => {
-    const messages = getAllMessages();
-    expect(messages.includes(getRandomMessage())).toBe(true);
+  afterEach(() => {
+    global.setInterval = originalSetInterval;
+    global.clearInterval = originalClearInterval;
   });
 
   it("returns a non-empty string", () => {
-    const message = getRandomMessage();
-    expect(message.length).toBeGreaterThan(0);
+    global.setInterval = ((callback: any, interval: any) => {
+      const message = callback();
+      expect(typeof message === "string").toBe(true);
+      expect(message.length).toBeGreaterThan(0);
+    }) as any;
+    getRandomMessage(1, 1);
   });
 
   it("returns a different message when called multiple times", () => {
-    const message1 = getRandomMessage();
-    const message2 = getRandomMessage();
-    const message3 = getRandomMessage();
-    expect(message1 === message2 && message2 === message3).toBe(false);
+    const messages: string[] = [];
+    global.setInterval = ((callback: any, interval: any) => {
+      for (let i = 0; i < 3; i++) {
+        messages.push(callback());
+      }
+    }) as any;
+    getRandomMessage(1, 3);
+    expect(messages[0] === messages[1] && messages[1] === messages[2]).toBe(
+      false
+    );
+  });
+
+  it("stops returning messages after the specified number of messages", () => {
+    const messages: string[] = [];
+    global.setInterval = ((callback: any, interval: any) => {
+      for (let i = 0; i < 4; i++) {
+        messages.push(callback());
+      }
+    }) as any;
+    getRandomMessage(1, 2);
+    expect(messages[2]).toBeUndefined();
+    expect(messages[3]).toBeUndefined();
+  });
+
+  it("clears the interval when the returned cancel function is called", () => {
+    let clearIntervalCalled = false;
+    let capturedIntervalId: NodeJS.Timer;
+
+    global.setInterval = ((callback: any, interval: any) => {
+      const intervalId = originalSetInterval(callback, interval);
+      capturedIntervalId = intervalId;
+      return intervalId;
+    }) as any;
+
+    global.clearInterval = ((intervalId: NodeJS.Timer) => {
+      originalClearInterval(intervalId);
+      if (intervalId === capturedIntervalId) {
+        clearIntervalCalled = true;
+      }
+    }) as any;
+
+    const cancelInterval = getRandomMessage(1, undefined);
+    cancelInterval();
+    expect(clearIntervalCalled).toBe(true);
   });
 
   it("does not throw an error when the array is non-empty", () => {
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,49 @@ export const getAllMessages = (): string[] => {
   return messages;
 };
 
-export const getRandomMessage = () => {
+export const getRandomMessage = (
+  duration: number = 3,
+  numberOfMessages?: number
+) => {
   const messages: string[] = getAllMessages();
   if (messages.length === 0) {
     throw new Error("No messages available");
   }
-  const randomMessage: string =
-    messages[Math.floor(Math.random() * messages.length)];
-  return randomMessage;
+
+  let usedMessages: string[] = [];
+  let currentIndex: number = 0;
+  let intervalId: NodeJS.Timeout | null = null;
+
+  const displayRandomLoadingMessage = () => {
+    // Clear interval if number of messages is defined and equals the current index
+    if (numberOfMessages !== undefined && currentIndex === numberOfMessages) {
+      if (intervalId) clearInterval(intervalId);
+      return;
+    }
+
+    // When all messages have been used, reset the index and usedMessages array
+    if (usedMessages.length === messages.length) {
+      usedMessages = [];
+      currentIndex = 0;
+    }
+
+    let randomMessage: string;
+
+    do {
+      randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    } while (usedMessages.includes(randomMessage));
+
+    usedMessages.push(randomMessage);
+    currentIndex++;
+
+    return randomMessage;
+  };
+
+  intervalId = setInterval(displayRandomLoadingMessage, duration * 1000);
+
+  return () => {
+    if (intervalId) clearInterval(intervalId);
+  };
 };
+
+export default { getAllMessages, getRandomMessage };
